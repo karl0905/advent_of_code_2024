8f30ed356765a2f3a94517556f8bcf2aa7f25a59
karl0905 <kalle44@live.dk>
2024-12-03 22:19:19 +0100

    day 2 part 1 (absolute warcrime)

--- 02/code.py
+++ 02/code.py
@@ -4,28 +4,54 @@ import pandas as pd
 with open('input.txt', 'r') as file:
     lines = file.readlines()
 
-# iterate over each line
 data = []
+# iterate over each line and split the line by whitespace
 for line in lines:
-    # split the line by whitespace, but keep the original structure
     parts = line.strip().split()
     data.append(parts)
 
 # create a DataFrame
 df = pd.DataFrame(data)
-
-# if you need to convert to numeric where possible:
 df = df.apply(pd.to_numeric, errors='coerce')
 
-# remove NaN values from each row, keeping only non-NaN values
+# remove NaN values from each row
 rows = [[value for value in row if not pd.isna(value)]
         for _, row in df.iterrows()]
 
+safe_reports = 0
 # check if row is increasing or decreasing
 for row in rows:
+    MIN_DIFF = 1
+    MAX_DIFF = 3
+
+    if row[0] < row[1]:
+        levels = "increasing"
+
+    elif row[0] > row[1]:
+        levels = "decreasing"
+    else:
+        continue
+
+    is_safe = True
+
     for i in range(len(row)-1):
         current_value = row[i]
         next_value = row[i + 1]
+        difference = abs(current_value - next_value)
 
-        if current_value < next_value and next_value - current_value >= 3:
-            print(f"{current_value} is less that {next_value} with less or exact 3")
+        if (levels == "increasing" and
+                current_value < next_value and
+                MIN_DIFF <= difference <= MAX_DIFF):
+            continue
+        elif (levels == "decreasing" and
+                current_value > next_value and
+                MIN_DIFF <= difference <= MAX_DIFF):
+            continue
+        else:
+            is_safe = False
+            break
+
+    if is_safe:
+        safe_reports += 1
+
+print(f"{safe_reports}")
